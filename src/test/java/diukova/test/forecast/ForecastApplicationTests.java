5b00b3aac0fd1a90023636e30c784687c59283cf
kseniia_diukova <diukova.ks@gmail.com>
2024-11-05 17:11:51 +1300

    fix tests

--- src/test/java/diukova/test/forecast/ForecastApplicationTests.java
+++ src/test/java/diukova/test/forecast/ForecastApplicationTests.java
@@ -1,16 +1,28 @@
 package diukova.test.forecast;
 
 import diukova.test.forecast.service.CityService;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Testcontainers
 class ForecastApplicationTests {
 
     @Autowired
     CityService cityService;
+
+    @DynamicPropertySource
+    static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", ContainersConfig::getJdbcUrl);
+        registry.add("spring.datasource.username", ContainersConfig::getUsername);
+        registry.add("spring.datasource.password", ContainersConfig::getPassword);
+    }
 
     @Test
     void contextLoads() {
@@ -25,4 +37,8 @@ class ForecastApplicationTests {
 
     //TODO add test for forecast request
 
+    @AfterAll
+    static void teardown() {
+        ContainersConfig.getPostgresContainer().stop();
+    }
 }
